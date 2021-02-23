b4ed5982ac220afd3b8b0261048bb67316c1b6ff
eduqg <eduardoqgomes@gmail.com>
2021-02-23 16:34:00 -0300

    Initial nlw4

---
+++ nlw4/frontmoveit/README.md
@@ -0,0 +1,32 @@
+# NLW 4 - Moveit
+
+Criado com 
+
+```console
+yarn create react-app frontmoveit --template typescript
+```
+
+```console
+
+
+```
+
+```console
+
+
+```
+
+```console
+
+
+```
+
+```console
+
+
+```
+
+```console
+
+
+```
---
+++ nlw4/frontmoveit/src/App.tsx
@@ -0,0 +1,13 @@
+import ExperienceBar from './components/ExperienceBar';
+import './styles/global.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
+
