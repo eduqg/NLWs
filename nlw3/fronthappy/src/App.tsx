8e4c00138a6d9b49cae91e675d588e1552829639
eduqg <eduardoqgomes@gmail.com>
2021-02-23 14:21:41 -0300

    nlw 3 happy front

---
+++ nlw3/fronthappy/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logoImg from './images/logo.svg'
+
+function App() {
+  return (
+   <div className="page-landing">
+     <div className="content-wrapper">
+
+     </div>
+   </div>
+  );
+}
+
+export default App;
